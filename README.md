e09dbf18cb2f67148d1190c479aeb4d5550cf09d
masterUNG <phrombutr@gmail.com>
2018-09-26 08:16:21 +0700

    Test

---
+++ README.md
@@ -0,0 +1,8 @@
+# widget_basic9
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var masterText = new Text('MasterUNG');
+    var masterCard = new Card(
+      color: Colors.white70,
+      child: Column(
+        children: <Widget>[Image.asset('assets/lion.png'), masterText],
+      ),
+    );
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('AppBar MasterUNG'),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Row(
+                  children: <Widget>[masterCard, masterCard, masterCard],
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[masterCard],
+                ),
+              ), Container(child: Row(children: <Widget>[masterCard, masterCard, masterCard],),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
